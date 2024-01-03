3ef038fd454e05e64465609ad99bf63bec0dde40
Ahmed.Zakieldin <ahmed.zakieldin@musala.com>
2024-01-03 14:26:45 +0200

    add project structure
    (cherry picked from commit 5c3959a8a50f95f0ffe2b73d351ae543d66edf99)

---
+++ book-info-service/src/test/java/soft/musala/bookinfoservice/BookInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.musala.bookinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
