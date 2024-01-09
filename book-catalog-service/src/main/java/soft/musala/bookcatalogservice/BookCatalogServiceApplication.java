a48314343939aaeba4f74cb59edba15f830e05e2
Ahmed.Zakieldin <ahmed.zakieldin@musala.com>
2024-01-09 09:36:05 +0200

    add timeout to rest template

--- book-catalog-service/src/main/java/soft/musala/bookcatalogservice/BookCatalogServiceApplication.java
+++ book-catalog-service/src/main/java/soft/musala/bookcatalogservice/BookCatalogServiceApplication.java
@@ -2,7 +2,9 @@ package soft.musala.bookcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -14,8 +16,11 @@ public class BookCatalogServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	@Bean
